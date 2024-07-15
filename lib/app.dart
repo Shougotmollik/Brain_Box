e17a96f6c7891a9238cca2a726f78ddebbe07b0e
Shougot mollik <shougotmollik@gmail.com>
2024-07-15 23:42:33 +0600

    start new project named Brain_Box

---
+++ lib/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BrainBoxApp extends StatelessWidget {
+  const BrainBoxApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:brain_box/app.dart';
+import 'package:flutter/cupertino.dart';
+
+void main(){
+  runApp(const BrainBoxApp());
+}

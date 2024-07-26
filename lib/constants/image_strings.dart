803bda557443da6be65a29a5d6de6a160eb3f73f
shougotmollik <shougotmollik@gmail.com>
2024-07-26 22:42:41 +0600

    add and design splash screen

--- lib/constants/image_strings.dart
+++ lib/constants/image_strings.dart
@@ -2,7 +2,7 @@ class AppImages {
   AppImages._();
 
   static const String appLogo = 'assets/icons/Logo.svg';
-  static const String onBoardingImage1 = 'assets/images/Robo Image.svg';
-  static const String onBoardingImage2 = 'Robot Image.svg';
-  static const String onBoardingImage3 = 'assets/images/Robot Img.svg';
+  static const String onBoardingImage1 = 'assets/images/RoboImage.svg';
+  static const String onBoardingImage2 = 'RobotImage.svg';
+  static const String onBoardingImage3 = 'assets/images/RobotImg.svg';
 }

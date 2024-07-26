e161906042ba3ab447cdb4579fb13087b5da62de
shougotmollik <shougotmollik@gmail.com>
2024-07-26 22:42:46 +0600

    add and design splash screen

--- lib/views/screens/onboarding_screen/onboarding_screen.dart
+++ lib/views/screens/onboarding_screen/onboarding_screen.dart
@@ -1,10 +1,26 @@
+import 'package:brain_box/constants/image_strings.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          PageView(
+            children: [
+              Column(
+                children: [
+                  SvgPicture.asset(AppImages.onBoardingImage1),
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }

e5635095ae9c1d8495f0b09594b65e58678f44f8
shougotmollik <shougotmollik@gmail.com>
2024-08-06 17:52:35 +0600

    add and design splash screen

--- lib/constants/image_strings.dart
+++ lib/constants/image_strings.dart
@@ -1,8 +1,12 @@
 class AppImages {
   AppImages._();
 
-  static const String appLogo = 'assets/icons/Logo.svg';
-  static const String onBoardingImage1 = 'assets/images/RoboImage.svg';
-  static const String onBoardingImage2 = 'RobotImage.svg';
-  static const String onBoardingImage3 = 'assets/images/RobotImg.svg';
+  static const String splashScreen =
+      'https://lottie.host/d98b6b1c-0bcb-4c6a-8e54-1feb9f58c885/rOVlMYYs89.json';
+  static const String onBoardingImage1 =
+      'https://lottie.host/6cf2c4a6-64ca-4dda-9b30-c79fe9054b1d/yVr99c52VK.json';
+  static const String onBoardingImage2 =
+      'https://lottie.host/a6a6342d-8ff6-45bf-9fd0-af3877d49ddb/Vx717ElrcU.json';
+  static const String onBoardingImage3 =
+      'https://lottie.host/90461dca-114e-4f6a-a5ff-610524247d9b/BGj6kPeCh8.json';
 }

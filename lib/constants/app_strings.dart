15fd56d6a39eb5987349905dc380decf53e729b0
shougotmollik <shougotmollik@gmail.com>
2024-08-06 17:52:31 +0600

    add and design splash screen

--- lib/constants/app_strings.dart
+++ lib/constants/app_strings.dart
@@ -2,4 +2,17 @@ class AppStrings {
   AppStrings._();
 
   static const String appName = "BrainBox";
+
+//   Onboarding Strings
+
+  static const String onboardingTitle1 = 'Unlock the Power Of  Future AI';
+  static const String onboardingTitle2 = 'Chat With Your Favourite Ai';
+  static const String onboardingTitle3 = 'Boost Your Mind Power with Ai';
+
+  static const String onboardingSubTitle1 =
+      'Chat with the smartest AI Future Experience power of AI with us';
+  static const String onboardingSubTitle2 =
+      'Chat with the smartest AI Future Experience power of AI with us';
+  static const String onboardingSubTitle3 =
+      'Chat with the smartest AI Future Experience power of AI with us';
 }

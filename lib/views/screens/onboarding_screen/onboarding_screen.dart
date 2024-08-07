7bd263a910e4aaae6a3840eb9c7651af3e51d72a
shougotmollik <shougotmollik@gmail.com>
2024-08-07 22:18:37 +0600

    add and design onboarding screen

--- lib/views/screens/onboarding_screen/onboarding_screen.dart
+++ lib/views/screens/onboarding_screen/onboarding_screen.dart
@@ -1,10 +1,27 @@
+import 'package:brain_box/controllers/onboarding_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final _contoller = OnboardingController();
+    return Scaffold(
+      body: PageView.builder(
+        itemCount: _contoller.onboardingPages.length,
+        itemBuilder: (context, index) {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Lottie.network(_contoller.onboardingPages[index].image),
+              Text(_contoller.onboardingPages[index].title),
+              Text(_contoller.onboardingPages[index].subTitle),
+            ],
+          );
+        },
+      ),
+    );
   }
 }
